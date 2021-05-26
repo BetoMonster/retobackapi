31770f19f7cbc51a6d853877e3214bc5922fdf95
BetoMonster <betomonster@gmail.com>
2021-05-26 00:25:15 -0500

    first commit create estructure

---
+++ src/models/posts.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose')
+
+const postsSchema = new mongoose.Schema({
+
+
+})
+
+
+const model = mongoose.model('posts', postSchema)
+
+module.exports = model
+
+
+
+
+{"content":"<p>I woke up this morning and I just wanted to show you ...</p>",
+"coverUrl":"https://dev-to-uploads.s3.amazonaws.com/uploads/articles/gq54hvxk91voat9qj90a.jpg",
+"creationDate":"20/04/2021 12:00:00",
+"creationTime":"15:00",
+"duration":"1 min",
+"likes":57,
+"newKey":"13",
+"postId":2,
+"tags":["#javaScript","#programming","#css"],
+"title":"WOW 😱 This pop-up is so nice","userId":3}
